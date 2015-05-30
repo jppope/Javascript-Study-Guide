44dc684eaa7276b15265df0545f40cdce60c2e02
Jon Paul Uritis <jonpaul.uritis@thestormcloudgroup.com>
2015-05-30 11:56:28 -0700

    started a list of resources and updated file names

---
+++ theBasics/resourceGuide.js
@@ -0,0 +1,16 @@
+
+// this json/ object is a list of resources online
+
+var resources = [	
+	{
+	"name": "Learn Javascript"	
+	"url": "http://www.learn-js.org/"
+	"description": "Basics of Javascript, exlplaination then code challenges"	
+	},
+	{
+	"name": "CodeCademy"	
+	"url": "http://www.codecademy.com/"
+	"description": "Learn how to program, a program of instruction and code challenges"	
+	}
+	
+]
