d230a2cbecb283b43e8eb1ec696b6857aac4abdd
Jon Paul Uritis <jonpaul.uritis@thestormcloudgroup.com>
2015-06-17 13:39:51 -0700

    couple of notes...dunno what to do with them

---
+++ theBasics/useful.js
@@ -0,0 +1,6 @@
+console.log = print out stuff that needs to be printed as part of the program
+console.dir = printing out to get the tree of an object or array
+console.debug = on / off switch to use for debugging
+JSON stringify function
+//normal plugin and play settings: 
+JSON.stringify(make, null, 4)	
