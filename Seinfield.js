49913037ae511ebede45862d69616157d947b69c
Jon Paul Uritis <jonpaul.uritis@thestormcloudgroup.com>
2015-06-23 18:42:37 -0700

    added Seinfield.js which is a reference to Seinfield Therory

---
+++ Seinfield.js
@@ -0,0 +1,7 @@
+//This document is being updated daily showing my progress... 
+//I started doing this a while ago at home but I figured its good to show progress and to update my github frequently
+
+
+var studyActivities = {};
+
+studyActivities.juneTwentyThree = {"durationHours":1, "topic": ["Passport.js", "Math.random(), concept and testing"] }
