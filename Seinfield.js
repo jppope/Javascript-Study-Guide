2dd7f6b573f6a40bfee4e9a1ed24906d278d5501
Jon Paul Uritis <jonpaul.uritis@thestormcloudgroup.com>
2015-06-24 08:57:18 -0700

    One hour of study

--- Seinfield.js
+++ Seinfield.js
@@ -1,7 +1,17 @@
 //This document is being updated daily showing my progress... 
 //I started doing this a while ago at home but I figured its good to show progress and to update my github frequently
 
+/*
+* Note: previous completed activities:
+* codecademy: html, CSS, Php, Javascript
+* codeschool: Javascript
+* LearnJS.org
+* nodeschool: Javascript Basics
+*/
+
 
 var studyActivities = {};
 
-studyActivities.juneTwentyThree = {"durationHours":1, "topic": ["Passport.js", "Math.random(), concept and testing"] }
+studyActivities.juneTwentyThree = {"durationHours":1, "topic": ["Passport.js", "Math.random(), concept and testing"]};
+studyActivities.juneTwentyFour = {"durationHours":1, "topic": ["Treehouse: conditional statements", "OpenSource Milage Log"]};
+
