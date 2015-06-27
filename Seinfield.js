d8ab0580d5f52b0774611ea3760b3e3580c0b6e6
Jon Paul Uritis <jonpaul.uritis@thestormcloudgroup.com>
2015-06-27 10:39:28 -0700

    functions and CoderByte Challenge #1

--- Seinfield.js
+++ Seinfield.js
@@ -15,3 +15,17 @@ var studyActivities = {};
 studyActivities.juneTwentyThree = {"durationHours":1, "topic": ["Passport.js", "Math.random(), concept and testing"]};
 studyActivities.juneTwentyFour = {"durationHours":1, "topic": ["Treehouse: conditional statements", "OpenSource Milage Log"]};
 studyActivities.juneTwentyFive = {"durationHours":1, "topic": ["Treehouse: Functions", "OpenSource Milage Log"]};
+studyActivities.juneTwentysix = {"durationHours":1, "topic": ["Treehouse: Functions"]};
+studyActivities.juneTwentySeven = {"durationHours":2, "topic": ["Treehouse: Functions", "CoderByte, First challenge"]};
+/***************************
+*CoderByte Solution:
+	function strSplit(str){
+	var string = "";
+	var thing =  str.split("");
+	for(var i = thing.length - 1; i >= 0 ; i--){
+	 string += thing[i];
+	}
+return string;
+}
+strSplit(str);
+****************************/
