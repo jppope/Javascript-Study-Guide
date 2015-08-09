3301333330b88b9cea7bb35e1814f63348703978
Jon Paul Uritis <jonpaul.uritis@thestormcloudgroup.com>
2015-08-08 23:57:21 -0700

    calculus, lat long, how to learn

--- Seinfield.js
+++ Seinfield.js
@@ -60,6 +60,7 @@ studyActivities.AugustFour = {"durationHours":2, "topic": ["Nightmarejs Scraping
 studyActivities.AugustFive = {"durationHours":4, "topic": ["Nightmarejs Scraping- did like 5 hours last night with steve"]};
 studyActivities.AugustSix = {"durationHours":0.5, "topic": ["bad day- did about 30 min on lat long square, lame"]};
 studyActivities.AugustSeven = {"durationHours":1, "topic": ["Calculus? Yea scared me too", "added a little to how to learn"]};
+studyActivities.AugustSeven = {"durationHours":1, "topic": ["Design work", "lat long javascript"]};
 
 
 
