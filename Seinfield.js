3c916b4b5d38511f6ce1269c0c0437b0f2b24f28
Jon Paul Uritis <jonpaul.uritis@thestormcloudgroup.com>
2015-12-15 23:18:55 -0800

    tons and tons of laravel

--- Seinfield.js
+++ Seinfield.js
@@ -107,6 +107,8 @@ studyActivities.September18 = { "durationHours":1, "topic": "javascript on treeh
 {"date": "tues dec 1 2015 23:11:51 GMT-0700 (PDT)}" "hours":1, "topic":"full laravel everything"},
 {"date": "sun dec 6 2015 23:11:51 GMT-0700 (PDT)}" "hours":.5, "topic":"misc opensource tools etc"},
 {"date": "sun dec 6 2015 23:11:51 GMT-0700 (PDT)}" "hours":2, "topic":"tons and tons of Laravel"},
+{"date": "tues dec 15 2015 23:11:51 GMT-0700 (PDT)}" "hours":2, "topic":"laravel up to the top of my head"},
+
 
 
 
