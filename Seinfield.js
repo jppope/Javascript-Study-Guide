7e99c94fbda903f21b571ed89f14becb6d8415aa
Jon Paul Uritis <jonpaul.uritis@thestormcloudgroup.com>
2016-01-03 21:53:10 -0800

    random

--- Seinfield.js
+++ Seinfield.js
@@ -111,6 +111,7 @@ studyActivities.September18 = { "durationHours":1, "topic": "javascript on treeh
 {"date": "tues dec 22 2015 23:11:51 GMT-0700 (PDT)}" "hours":2, "topic":"D3.js"},
 {"date": "sun dec 27 2015 23:11:51 GMT-0700 (PDT)}" "hours":2, "topic":"theAcademy development"},
 {"date": "mon dec 28 2015 23:11:51 GMT-0700 (PDT)}" "hours":2, "topic":"d3js"},
+{"date": "mon dec 28 2015 23:11:51 GMT-0700 (PDT)}" "hours":1, "topic":"various"},
 
 
 
