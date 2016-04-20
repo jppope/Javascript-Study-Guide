0fbb079089b0b4216d90769a4931b848489a7d3c
Jon Paul Uritis <jonpauluritis@Jons-MacBook-Pro-3.local>
2016-04-19 22:01:25 -0700

    first post in awhile

--- Seinfield.js
+++ Seinfield.js
@@ -126,8 +126,8 @@ studyActivities.September18 = { "durationHours":1, "topic": "javascript on treeh
 {"date": "Mon Feb 2 2016 23:11:51 GMT-0700 (PDT)}" "hours":1, "topic":"Laravel project flyer!"},
 {"date": "Mon Feb 2 2016 23:11:51 GMT-0700 (PDT)}" "hours":1, "topic":"Server stuff"},
 {"date": "Thurs Feb 11 2016 23:11:51 GMT-0700 (PDT)}" "hours":1, "topic":"Mongoosejs and MongoDB"},
-{"date": "today", some laravel... you know it"}
-
+{"date": "today", "some laravel... you know it"}
+{"date": "April 17 2016 23:11:51 GMT-0700 (PDT)}" "hours":1, "topic":"ACL Laravel, laracast"},
 
 
 
