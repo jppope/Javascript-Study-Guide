ba7246c127758e26366a3be7172a9eabd2629a75
Jon Paul Uritis <jonpaul@lojistic.com>
2017-06-22 13:58:16 -0700

    building my own underscore

--- functionalProgramming/reduce/reduce-example4.js
+++ functionalProgramming/reduce/reduce-example4.js
@@ -1,7 +1,56 @@
 
 // simple example
 // I might replace example one with it
-
-var arr = [1,5,7,9,7,5];
+const arr = [1,5,7,9,7,5];
 var total = arr.reduce((total,value) => total + value);
-console.log(total)
+
+
+/**
+ * Why not rebuild underscores.js
+ */
+
+
+/**
+ * find average using reduce
+ */
+function average(arr){
+  console.log("====== average ======");
+  console.log(arr);
+  return arr.reduce((total, amount, index, arr) => {
+    total += amount;
+    if (index === arr.length-1) {
+      return total/arr.length
+    } else {
+      return total
+    }
+  });
+}
+console.log(average(arr)+ '\n')
+
+/**
+ * reduce to unique values ALSO example of filter
+ */
+function uniqes(arr) {
+  console.log("====== uniques ======");
+    console.log(arr);
+    return arr.reduce((total, amount, index) => {
+      if(total.indexOf(amount) < 0){ total.push(amount); }
+      return total;
+    }, [])
+}
+let val = uniqes(arr);
+console.log(val)
+
+/**
+ * example of map via reduce method
+ */
+const map = arr.reduce((total, amount, index,arr) =>{
+  if(amount > 5) total.push(amount);
+  if(index = arr.length-1) return total;
+},[]);
+//console.log(map);
+
+
+
+
+
